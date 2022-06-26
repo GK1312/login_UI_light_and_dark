9d853eb2a225782366625a65aa1752ad4b4bde84
GK1312 <gauravsk1234@gmail.com>
2022-06-26 19:35:16 +0530

    basic theme data created

---
+++ lib/themes/app_theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:login_ui_dark_light/themes/dark_theme.dart';
+import 'package:login_ui_dark_light/themes/light_theme.dart';
+
+class AppTheme {
+  static ThemeData light = lightTheme;
+  static ThemeData dark = darkTheme ;
+}
