10867dd4b78c6ea0c07c43e3ff5219c1b8ef55a1
GK1312 <gauravsk1234@gmail.com>
2022-06-26 20:40:48 +0530

    advanced theme changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:login_ui_dark_light/screens/login.dart';
 import 'package:login_ui_dark_light/themes/app_theme.dart';
+import 'package:login_ui_dark_light/themes/theme_model.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Login Page UI',
-      theme: AppTheme.light,
-      darkTheme: AppTheme.dark,
-      themeMode: ThemeMode.system,
-      home: const LoginScreen(),
-    );
-  }
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => ThemeModel(),
+      child: Consumer<ThemeModel>(
+        builder: (context, ThemeModel themeNotifier, child) {
+          return MaterialApp(
+            home: const LoginScreen(),
+            theme: themeNotifier.isDark ? AppTheme.dark : AppTheme.light,
+            debugShowCheckedModeBanner: false,
+          );
+        },
+      ),
+    ),
+  );
 }
--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,138 +1,172 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/material.dart';
+import 'package:login_ui_dark_light/themes/theme_model.dart';
+import 'package:provider/provider.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
   @override
-  State<LoginScreen> createState() => _LoginScreenState();
+  // ignore: library_private_types_in_public_api
+  _LoginScreenState createState() => _LoginScreenState();
 }
 
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
+    final Size size = MediaQuery.of(context).size;
 
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Container(
-          width: size.width,
-          height: size.height,
-          padding: const EdgeInsets.only(
-            left: 20,
-            right: 20,
-            top: 150,
-            bottom: 80,
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                "Hello, \nWelcome Back",
-                style: Theme.of(context).textTheme.headline1,
+    return Consumer<ThemeModel>(
+      builder: (context, ThemeModel themeNotifier, child) {
+        return Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            title: Text(
+              themeNotifier.isDark ? "Dark Mode" : "Light Mode",
+              style: TextStyle(
+                color:
+                    themeNotifier.isDark ? Colors.white : Colors.grey.shade900,
               ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                      Image(
-                        width: 30,
-                        image: AssetImage('assets/icons/google.png'),
-                      ),
-                      SizedBox(
-                        width: 40,
-                      ),
-                      Image(
-                        width: 30,
-                        image: AssetImage('assets/icons/facebook.png'),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 50,
-                  ),
-                  Container(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 20,
-                      vertical: 5,
-                    ),
-                    decoration: BoxDecoration(
-                      color: Theme.of(context).primaryColorLight,
-                      borderRadius: const BorderRadius.all(
-                        Radius.circular(20),
-                      ),
-                    ),
-                    child: const TextField(
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: "Email or Phone Number",
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Container(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 20,
-                      vertical: 5,
-                    ),
-                    decoration: BoxDecoration(
-                      color: Theme.of(context).primaryColorLight,
-                      borderRadius: const BorderRadius.all(
-                        Radius.circular(20),
-                      ),
-                    ),
-                    child: const TextField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: "Password",
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Text(
-                    "Forgot Password?",
-                    style: Theme.of(context).textTheme.bodyText1,
-                  ),
-                ],
+            ),
+            actions: [
+              IconButton(
+                icon: Icon(
+                    themeNotifier.isDark
+                        ? Icons.nightlight_round
+                        : Icons.wb_sunny,
+                    color: themeNotifier.isDark
+                        ? Colors.white
+                        : Colors.grey.shade900),
+                onPressed: () {
+                  themeNotifier.isDark
+                      ? themeNotifier.isDark = false
+                      : themeNotifier.isDark = true;
+                },
               ),
-              Column(
-                children: [
-                  RaisedButton(
-                    padding: const EdgeInsets.all(20),
-                    elevation: 0,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    onPressed: () {},
-                    child: const Center(
-                      child: Text(
-                        'Login',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  Text(
-                    "Create Account",
-                    style: Theme.of(context).textTheme.bodyText1,
-                  )
-                ],
-              )
             ],
           ),
-        ),
-      ),
+          body: SingleChildScrollView(
+            child: Container(
+              width: size.width,
+              height: size.height,
+              padding: EdgeInsets.only(
+                left: 20,
+                right: 20,
+                bottom: size.height * 0.2,
+                top: size.height * 0.05,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Hello, \nWelcome Back",
+                    style: Theme.of(context).textTheme.headline1?.copyWith(
+                          fontSize: size.width * 0.1,
+                        ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          Image(
+                            width: 30,
+                            image: AssetImage('assets/icons/google.png'),
+                          ),
+                          SizedBox(width: 40),
+                          Image(
+                            width: 30,
+                            image: AssetImage('assets/icons/facebook.png'),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 50,
+                      ),
+                      Container(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 20,
+                          vertical: 5,
+                        ),
+                        decoration: BoxDecoration(
+                          color: Theme.of(context).primaryColorLight,
+                          borderRadius: const BorderRadius.all(
+                            Radius.circular(20),
+                          ),
+                        ),
+                        child: const TextField(
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: "Email or Phone number",
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Container(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 20,
+                          vertical: 5,
+                        ),
+                        decoration: BoxDecoration(
+                          color: Theme.of(context).primaryColorLight,
+                          borderRadius: const BorderRadius.all(
+                            Radius.circular(20),
+                          ),
+                        ),
+                        child: const TextField(
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: "Password",
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        "Forgot Password?",
+                        style: Theme.of(context).textTheme.bodyText1,
+                      )
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      RaisedButton(
+                        onPressed: () => {},
+                        elevation: 0,
+                        padding: const EdgeInsets.all(18),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: const Center(
+                          child: Text(
+                            "Login",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      Text(
+                        "Create account",
+                        style: Theme.of(context).textTheme.bodyText1,
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        );
+      },
     );
   }
 }
